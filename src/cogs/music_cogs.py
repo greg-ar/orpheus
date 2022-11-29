c424ef8b98b35fe7d9925c45ea3abf6689ae4bbb
Greg <gregoryarapis@gmail.com>
2022-11-29 20:38:52 +0200

    Bugfix: Would not join and play

--- src/cogs/music_cogs.py
+++ src/cogs/music_cogs.py
@@ -28,12 +28,15 @@ class music(commands.Cog):
     @commands.command()
     async def leave(self,ctx):
         await ctx.voice_client.disconnect()
+        self.is_playing = False
+        self.current_voice_channel = None
+
         
     @commands.command()
     async def play(self,ctx,url):
         if ctx.author.voice.channel is not self.current_voice_channel:
             await self.join(ctx)
-            await asyncio.sleep(0.5) #The bots needs to wait for the connection to initialize otherwise it thinks that it isn't connected to VC
+            await asyncio.sleep(0.5) #The bot needs to wait for the connection to initialize otherwise it thinks that it isn't connected to VC
         if self.is_playing :
             ctx.voice_client.stop()
         ctx.voice_client.play(await yt_load(url))
